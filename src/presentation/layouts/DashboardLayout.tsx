79a58b723381d8cfc2a6ac540aa95676362d3651
Facundo Herrera <facundhfed@gmail.com>
2024-02-09 10:51:54 +0100

    Routes working

---
+++ src/presentation/layouts/DashboardLayout.tsx
@@ -0,0 +1,13 @@
+
+import { Outlet } from 'react-router-dom'
+
+const DashboardLayout = () => {
+    return (
+        <>
+            <h1>DashboardLayout</h1>
+            <Outlet />
+        </>
+    )
+}
+
+export default DashboardLayout
---
+++ src/presentation/pages/audio-to-text/AudioToTextPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AudioToTextPage = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default AudioToTextPage
