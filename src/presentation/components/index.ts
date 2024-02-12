9928b2ec7067d363201fb3e616665463ef24fea4
Facundo Herrera <facundhfed@gmail.com>
2024-02-12 12:10:45 +0100

    Working with orthograpy screen

---
+++ src/presentation/components/index.ts
@@ -0,0 +1,5 @@
+export * from "./chat-bubbles/GptMessages";
+export * from "./chat-bubbles/MyMessages";
+export * from "./sidebar/SideBarMenuItem";
+export * from "./loaders/TypingLoader";
+export * from "./chat-input-boxes/TextMessageBox";
--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,16 +1,53 @@
-import { GptMessages, MyMessages } from '../../components/chat-bubbles'
-import { TypingLoader } from '../../components/loaders/TypingLoader'
+import { useState } from 'react'
+import { TypingLoader, TextMessageBox, GptMessages, MyMessages } from '../../components'
+
+interface Message {
+    text: string;
+    isGptMessage: boolean;
+}
 
 export const OrthographyPage = () => {
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [messages, setMessages] = useState<Message[]>([])
+
+    const handlePost = async (text: string) => {
+        setIsLoading(true);
+        setMessages((prev) => [...prev, { text: text, isGptMessage: false }]);
+
+        //TODO Use case
+
+        setIsLoading(false);
+
+        // Todo añadir el mensaje isGptMessage en true
+    }
     return (
         <div className='chat-container'>
             <div className='chat-messages'>
                 <div className='grid grid-cols-12 gap-y-2'>
                     <GptMessages text='Hola, puedes escribir tu texto en español, y te ayudo con las correcciones' />
-                    <MyMessages text='Hola' />
-                    <TypingLoader className='fade-in' />
+
+                    {
+                        messages.map((message, index) => (
+                            message.isGptMessage ? <GptMessages key={index} text='Open AI message' /> : <MyMessages key={index} text={message.text} />
+                        ))
+                    }
+
+
+                    {
+                        isLoading && (
+                            <div className='col-start-1 col-end-12 fade-in'>
+                                <TypingLoader />
+                            </div>
+                        )
+                    }
+
+
+
                 </div>
             </div>
+
+            <TextMessageBox onSendMessage={handlePost} placeholder='Escribe tu texto aquí' disableCorrections />
         </div>
     )
 }
